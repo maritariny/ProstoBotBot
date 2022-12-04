34234b39e2e55c22a1f2a652b4c64dc733437422
Marina Malygina <marina0391@mail.ru>
2022-12-04 18:25:26 +0300

    урок 9

--- node/src/main/java/ru/malm/service/MainService.java
+++ node/src/main/java/ru/malm/service/MainService.java
@@ -5,4 +5,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 // Сервис для обработки входящих сообщений. Дальше будут вызываться специфические серисы в зависимости от типа сообщения
 public interface MainService {
     void processTextMessage(Update update);
+    void processDocMessage(Update update);
+    void processPhotoMessage(Update update);
 }
--- node/src/main/java/ru/malm/service/impl/MainServiceImpl.java
+++ node/src/main/java/ru/malm/service/impl/MainServiceImpl.java
@@ -1,9 +1,9 @@
 package ru.malm.service.impl;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
 import ru.malm.dao.AppUserDAO;
 import ru.malm.dao.RawDataDAO;
 import ru.malm.entity.AppUser;
@@ -12,8 +12,11 @@ import ru.malm.service.MainService;
 import ru.malm.service.ProducerService;
 
 import static ru.malm.entity.enums.UserState.BASIC_STATE;
+import static ru.malm.entity.enums.UserState.WAIT_FOR_EMAIL_STATE;
+import static ru.malm.service.enums.ServiceCommands.*;
 
 @Service
+@Log4j
 public class MainServiceImpl implements MainService {
     private final RawDataDAO rawDataDAO;
     private final ProducerService producerService;
@@ -28,22 +31,106 @@ public class MainServiceImpl implements MainService {
     @Override
     public void processTextMessage(Update update) {
         saveRawData(update);
+        var appUser = findOrSaveAppUser(update);
+        var userState = appUser.getState();
+        var text = update.getMessage().getText();
+        var output = "";
 
-        // Разбор входящего update на составные части, доставание телеграмовского юзера
-        var textMessage = update.getMessage();
-        var telegramUser = textMessage.getFrom();
-        var appUser = findOrSaveAppUser(telegramUser);
+        if (CANCEL.equals(text)) {
+            // Отменяет текущую команду и сбрасывает state пользователя к базовому состоянию
+            output = cancelProcess(appUser);
+        } else if (BASIC_STATE.equals(userState)) {
+            // Ожидание ввода сервисных команд
+            output = processServiceCommand(appUser, text);
+        } else if (WAIT_FOR_EMAIL_STATE.equals(userState)) {
+            //TODO: добавить обработку емейла
+        } else {
+            log.error("Unknown user state: " + userState);
+            output = "Неизвестная ошибка! Введите /cancel и попробуйте снова!";
+        }
 
-        var message = update.getMessage();
+        var chatId = update.getMessage().getChatId();
+        sendAnswer(output, chatId);
+    }
+
+    @Override
+    public void processDocMessage(Update update) {
+        saveRawData(update);
+        var appUser = findOrSaveAppUser(update);
+        var chatId = update.getMessage().getChatId();
+        if (isNotAllowToSendContent(chatId, appUser)) {
+            return;
+        }
+
+        //TODO добавить сохранение документа
+        var answer = "Документ успешно загружен! Ссылка для скачивания: http://test.ru/get-doc/???";
+        sendAnswer(answer, chatId);
+    }
+
+    private boolean isNotAllowToSendContent(Long chatId, AppUser appUser) {
+        var userState = appUser.getState();
+        if (!appUser.getIsActive()) {
+            var error = "Зарегистрируйтесь или активируйте свою учетную запись для загрузки контента.";
+            sendAnswer(error, chatId);
+            return true;
+        } else if (!BASIC_STATE.equals(appUser.getState())) {
+            var error = "Отмените текущую команду с помощью /cancel для отправки файлов.";
+            sendAnswer(error, chatId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void processPhotoMessage(Update update) {
+        saveRawData(update);
+        var appUser = findOrSaveAppUser(update);
+        var chatId = update.getMessage().getChatId();
+        if (isNotAllowToSendContent(chatId, appUser)) {
+            return;
+        }
+
+        //TODO добавить сохранение документа
+        var answer = "Фото успешно загружено! Ссылка для скачивания: http://test.ru/get-photo/???";
+        sendAnswer(answer, chatId);
+    }
+
+    private void sendAnswer(String output, Long chatId) {
         var sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId().toString());
-        sendMessage.setText("Hello from NODE");
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(output);
         producerService.producerAnswer(sendMessage);
     }
 
-    private AppUser findOrSaveAppUser(User telegramUser) {
+    private String processServiceCommand(AppUser appUser, String text) {
+        if (REGISTRATION.equals(text)) {
+            //TODO добавить регистрацию
+            return "Временно недоступно.";
+        } else if (HELP.equals(text)) {
+            return help();
+        } else if (START.equals(text)) {
+            return "Приветствую! Чтобы посмотреть список доступных команд, введите /help";
+        } else {
+            return "Неизвестная команда! Чтобы посмотреть список доступных команд, введите /help";
+        }
+    }
+
+    private String help() {
+        return "Список доступных команд: \n"
+                + "/cancel - отмена выполнения текущей команды;\n"
+                + "/registration - регистрация пользователя.";
+    }
+
+    private String cancelProcess(AppUser appUser) {
+        appUser.setState(BASIC_STATE);
+        appUserDAO.save(appUser);
+        return "Команда отменена!";
+    }
+
+    private AppUser findOrSaveAppUser(Update update) {
         // persistent = Объект есть в БД, имеет заполненный первичный ключ и связан с Hibernate
         // transient = Объекта еще нет в БД, необходимо его сохранить
+        var telegramUser = update.getMessage().getFrom();
         AppUser persistentAppUser = appUserDAO.findAppUserByTelegramUserId(telegramUser.getId());
 
         if (persistentAppUser == null) {
